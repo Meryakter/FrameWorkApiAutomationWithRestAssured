8be3caafcb16c8d3e0bd26c53aa2dd2a57be7d82
Meryakter <meryakter@aol.com>
2023-12-07 23:23:32 -0600

    second attempt with jenkins tests

--- src/test/java/com/izaan/apitests/UserLoginTest.java
+++ src/test/java/com/izaan/apitests/UserLoginTest.java
@@ -49,7 +49,7 @@ public class UserLoginTest {
 
 @Test
     public void invaliduserLoginAPI(){
-    Response resp=  UserLogin.loginUserNegativeUsername()G;
+    Response resp=  UserLogin.loginUserNegativeUsername();
     BaseAssertion.verifySpecificMessage(resp, "statusCode", "400");
     BaseAssertion.verifySpecificMessage(resp,"message","Incorrect username or password.");
 
